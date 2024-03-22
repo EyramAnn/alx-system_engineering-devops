bf5cf6e88813516a852de01c4f84ffc3a48e9e76
EyramAnn <anndekonor@gmail.com>
2024-03-22 15:22:01 -0700

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# Using Puppet, install flask from pip3.
+#!/usr/bin/pup
+# Using puppet, install flask from pip3.
 
-package {'flask':
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
